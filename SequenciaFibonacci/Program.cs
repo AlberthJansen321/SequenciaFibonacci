6d612777650e5888e4f9a9f7840a9db07b757e23
AlberthJansen321 <75434559+AlberthJansen321@users.noreply.github.com>
2022-05-01 11:53:14 -0300

    Adicionar arquivos de projeto.

---
+++ SequenciaFibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequenciaFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, c = 0;
+            Console.WriteLine("Digite a quantidade de valores");
+            int valores = int.Parse(Console.ReadLine());
+
+            for(int i=0; i < valores; i++)
+            {
+                if(i < (valores - 1))
+                {
+                    Console.Write($"{a}, ");
+                }
+                else
+                {
+                    Console.Write($"{a}");
+                }
+
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
